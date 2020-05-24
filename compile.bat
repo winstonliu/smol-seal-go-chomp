2efda75f56709c5da479c57617c6829cff1dcd3a
Winston Liu <winston.liu@mail.utoronto.ca>
2020-05-24 16:52:11 -0700

    Final changes to compilation file

--- compile.bat
+++ compile.bat
@@ -1,4 +1,5 @@
 @echo off
 pyinstaller --icon=assets\seal.ico -w -n smol_seal_go_chomp main.py
+timeout /t 5
 xcopy FreeSansBold.ttf dist\smol_seal_go_chomp\
 xcopy /e assets dist\smol_seal_go_chomp\assets\
